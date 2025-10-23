d9c3576bddafb04c1cf360f0cef9e608d8c3ecf7
Ridhamsood12 <soodridham@gmail.com>
2025-10-23 01:23:45 -0500

    feat: Added the observer pattern to the client class.

--- client/client.py
+++ client/client.py
@@ -1,11 +1,14 @@
 """This file contains the information of the client."""
 
 __name__ = "Ridham Sood"
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 
 from email_validator import EmailNotValidError, validate_email
+from patterns.observer.observer import Observer
+from utility.file_utils import simulate_send_email
+import datetime
 
-class Client:
+class Client(Observer):
     """Initializes a client object."""
 
     def __init__(self, client_number:int, first_name:str, last_name: str,
@@ -50,21 +53,56 @@ class Client:
         
     @property
     def client_number(self) -> int:
+        """This helps to access the client number anywhere.
+        
+        Return:
+        int: Returns the client number
+        """
         return self.__client_number
     
     @property
     def first_name(self) -> str:
+        """This helps to access the first name of the client anywhere.
+        
+        Return:
+        str: Returns the first name of the client
+        """
         return self.__first_name
     
     @property
     def last_name(self) -> str:
+        """This helps to access the last name of the client anywhere.
+        
+        Return:
+        str: Returns the last name of the client.
+        """
         return self.__last_name
     
     @property
     def email_address(self) -> str:
+        """This helps to access the email address of the client anywhere.
+        
+        Return:
+        str: Returns the email address of the client
+        """
         return self.__email_address
     
     def __str__(self) -> str:
+        """This method returns the formatted string.
+        
+        Return:
+        str: Return the formatted string.
+        """
         return(f"{self.__last_name}, {self.__first_name} [{self.__client_number}] - {self.__email_address}")
         
-
+    def update(self, message: str) -> None:
+        """This method updates the message.
+        
+        Args
+        message(str): Represents the message to be shown.
+        """
+        subject = f"Alert: Unusual Activity: {datetime.now()}"
+        message = (f"Notification for {self.__client_number}: {self.__first_name}"
+                    +f"{self.__last_name}: {message}")
+        
+        simulate_send_email(self.email_address, subject, message)
