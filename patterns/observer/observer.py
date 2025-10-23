a4b8ba62a78cd14352a8884cde3033bb0af36dc9
Ridhamsood12 <soodridham@gmail.com>
2025-10-23 00:21:39 -0500

    feat: Added the observer superclass.

---
+++ patterns/observer/observer.py
@@ -0,0 +1,20 @@
+"""This file contains the interface which will notify the changes."""
+
+__author__ = "Ridham Sood"
+__version__ = "1.0.0"
+
+from abc import ABC, abstractmethod
+
+class Observer(ABC):
+    """This class constructs an interface which will notify the changes
+    made in the subject."""
+
+    @abstractmethod
+    def update(self, message: str):
+        """This method will update the changes.
+        
+        Args:
+        message(str): Represents the message to be updated
+        """
+
+        pass
